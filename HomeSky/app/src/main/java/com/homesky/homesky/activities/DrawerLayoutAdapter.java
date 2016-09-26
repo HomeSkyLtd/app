710cbd0f880b33c4504df4ee02a037b46b74b201
Henrod <henrique.93.rodrigues@gmail.com>
2016-09-26 17:04:06 -0300

    addin footer to drawer

--- HomeSky/app/src/main/java/com/homesky/homesky/activities/DrawerLayoutAdapter.java
+++ HomeSky/app/src/main/java/com/homesky/homesky/activities/DrawerLayoutAdapter.java
@@ -12,6 +12,9 @@ import android.widget.TextView;
 
 import com.homesky.homesky.R;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by henrique on 9/23/16.
  */
@@ -25,10 +28,18 @@ class DrawerLayoutAdapter extends BaseAdapter {
 
     private static LayoutInflater sInflater = null;
     private String[] mModules;
+    final private Map<String, Integer> mapToIcon;
 
     DrawerLayoutAdapter(Context context, String[] modules) {
         sInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         mModules = modules;
+
+        mapToIcon = new HashMap<>();
+        mapToIcon.put("State", R.drawable.ic_poll_white_24dp);
+        mapToIcon.put("Rules", R.drawable.ic_library_books_white_24dp);
+        mapToIcon.put("User", R.drawable.ic_person_white_24dp);
+        mapToIcon.put("Controller", R.drawable.ic_router_white_24dp);
+        mapToIcon.put("Notification", R.drawable.ic_message_white_24dp);
     }
 
     @Override
@@ -53,8 +64,8 @@ class DrawerLayoutAdapter extends BaseAdapter {
         if (view == null)
             view = sInflater.inflate(R.layout.drawer_list_item, null);
 
-        /*ImageView image = (ImageView) view.findViewById(R.id.drawer_list_icon);
-        image.setImageResource(R.drawable.ic_poll_white_24dp);*/
+        ImageView image = (ImageView) view.findViewById(R.id.drawer_list_icon);
+        image.setImageResource(mapToIcon.get(mModules[i]));
 
         TextView text = (TextView) view.findViewById(R.id.drawer_list_title);
         text.setText(mModules[i]);
--- HomeSky/app/src/main/java/com/homesky/homesky/activities/MenuFragmentsActivity.java
+++ HomeSky/app/src/main/java/com/homesky/homesky/activities/MenuFragmentsActivity.java
@@ -14,6 +14,7 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -97,6 +98,14 @@ public class MenuFragmentsActivity extends AppCompatActivity {
         mModulesTitles = getResources().getStringArray(R.array.modules_titles);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.menu_fragments_activity_left_drawer);
+
+        ViewGroup footer = (ViewGroup) getLayoutInflater()
+                .inflate(
+                    R.layout.drawer_footer,
+                    mDrawerList, false
+                );
+        mDrawerList.addFooterView(footer, null, false);
+
         mDrawerList.setAdapter(new DrawerLayoutAdapter(this, mModulesTitles));
         mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
