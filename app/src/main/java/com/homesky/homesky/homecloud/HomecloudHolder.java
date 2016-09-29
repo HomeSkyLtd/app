20e1d5746abb80071342a9d0a94a1eb0341aa757
Henrod <henrique.93.rodrigues@gmail.com>
2016-09-29 20:04:37 -0300

    refactoring code structure

---
+++ app/src/main/java/com/homesky/homesky/homecloud/HomecloudHolder.java
@@ -0,0 +1,49 @@
+package com.homesky.homesky.homecloud;
+
+import android.content.Context;
+
+import com.homesky.homecloud_lib.Homecloud;
+
+public class HomecloudHolder {
+    private static Homecloud instance = null;
+
+    private static String url;
+    private static String username;
+    private static String password;
+    private static String token;
+
+    public static void setUrl(String url) {
+        HomecloudHolder.url = url;
+    }
+
+    public static void setUsername(String username) {
+        HomecloudHolder.username = username;
+        if(instance != null)
+            instance.setUsername(username);
+    }
+
+    public static void setPassword(String password) {
+        HomecloudHolder.password = password;
+        if(instance != null)
+            instance.setPassword(password);
+    }
+
+    public static void setToken(String token) {
+        HomecloudHolder.token = token;
+        if(instance != null)
+            instance.setToken(token);
+    }
+
+    public static Homecloud getInstance(){
+        if(instance == null) {
+            instance = new Homecloud.Builder()
+                    .url(url)
+                    .username(username)
+                    .password(password)
+                    .token(token)
+                    .build();
+
+        }
+        return instance;
+    }
+}
