ec6b89f20d273640896613b097606aead795e607
Henrod <henrique.93.rodrigues@gmail.com>
2016-11-01 15:57:18 -0200

    logout a melhorar

--- app/src/main/java/com/homesky/homesky/fragments/settings/SettingsFragment.java
+++ app/src/main/java/com/homesky/homesky/fragments/settings/SettingsFragment.java
@@ -6,8 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.homesky.homecloud_lib.model.response.SimpleResponse;
 import com.homesky.homesky.R;
+import com.homesky.homesky.command.LogoutCommand;
+import com.homesky.homesky.login.LoginActivity;
+import com.homesky.homesky.request.AsyncRequest;
+import com.homesky.homesky.request.RequestCallback;
 
 /**
  * Created by henrique on 9/27/16.
@@ -16,10 +22,29 @@ import com.homesky.homesky.R;
 public class SettingsFragment extends Fragment {
     public SettingsFragment() {}
 
+    private Button mLogoutButton;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        mLogoutButton = (Button) view.findViewById(R.id.settings_fragment_button_logout);
+        mLogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AsyncRequest(new LogoutCallback())
+                        .execute(new LogoutCommand());
+            }
+        });
+
         return view;
+    }
+
+    private class LogoutCallback implements RequestCallback {
+        @Override
+        public void onPostRequest(SimpleResponse s) {
+            startActivity(LoginActivity.newIntent(getActivity(), LoginActivity.LoginAction.LOGIN));
+        }
     }
 }
