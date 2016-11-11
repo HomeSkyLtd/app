b50e3e519022994d88667cf40940af2a0525d253
Fabio Muramatsu <fabio.muramatsu@outlook.com>
2016-11-11 18:32:57 +0000

    Added splash screen

---
+++ app/src/main/java/com/homesky/homesky/activities/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.homesky.homesky.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.homesky.homesky.login.LoginActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
