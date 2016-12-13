0ef38a4565c263d84357099686d826e4192cb5ac
Fabio Muramatsu <fabio.muramatsu@outlook.com>
2016-12-13 21:12:04 +0000

    Added ability to specify server

--- app/src/main/java/com/homesky/homesky/login/LoginFragment.java
+++ app/src/main/java/com/homesky/homesky/login/LoginFragment.java
@@ -11,9 +11,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.iid.FirebaseInstanceId;
@@ -30,25 +33,27 @@ import com.homesky.homesky.user.UserActivity;
 public class LoginFragment extends Fragment implements RequestCallback {
 
     private static final String TAG = "LoginFragment";
-    //private static final String URL = "http://192.168.0.197:3000";             // Henrique no energia
-    //private static final String URL = "http://10.144.59.104:3000";    // Henrique no Eduroam
-    private static final String URL = "http://192.168.1.108:3000";      // Casa do Henrique
-    //private static final String URL = "http://ec2-52-67-169-17.sa-east-1.compute.amazonaws.com:3000"; // AWS do Henrique
-    //private static final String URL = "http://ec2-52-67-3-31.sa-east-1.compute.amazonaws.com:3000"; // AWS do Fabão
-    //private static final String URL = "http://192.168.1.116:3000";      // Scopus
+
+    private static final String[] addresses = {
+            "http://ec2-52-67-3-31.sa-east-1.compute.amazonaws.com:3000", //AWS Fabio
+            "http://ec2-52-67-171-212.sa-east-1.compute.amazonaws.com:3000" //AWS Ricardo
+    };
+    private String mSelectedAddress = addresses[0];
 
     private Button mLoginButton;
     private Button mSigninButton;
     private EditText mEditTextLogin;
     private EditText mEditTextPassword;
     private CheckBox mRememberMe;
+    private Spinner mServerAddressSpinner;
+    private EditText mServerAddressEditText;
 
     public LoginFragment() {}
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        HomecloudHolder.getInstance().setUrl(URL);
+        //HomecloudHolder.getInstance().setUrl(URL);
         String token = FirebaseInstanceId.getInstance().getToken();
 
         if (token != null)
@@ -79,12 +84,41 @@ public class LoginFragment extends Fragment implements RequestCallback {
         mEditTextLogin = (EditText) view.findViewById(R.id.login_fragment_edit_login);
         mEditTextPassword = (EditText) view.findViewById(R.id.login_fragment_edit_passwd);
 
+        mServerAddressEditText = (EditText) view.findViewById(R.id.login_fragment_server_address_edit_text);
+        mServerAddressEditText.setVisibility(View.GONE);
+
+        mServerAddressSpinner = (Spinner)view.findViewById(R.id.login_fragment_server_spinner);
+        mServerAddressSpinner.setAdapter(new ArrayAdapter<String>(
+                getActivity(), android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.server_options)
+        ));
+        mServerAddressSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                if(position < addresses.length) {
+                    mServerAddressEditText.setVisibility(View.GONE);
+                    mSelectedAddress = addresses[position];
+                }
+                else {
+                    mServerAddressEditText.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
         mLoginButton = (Button) view.findViewById(R.id.login_fragment_login_button);
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String login = mEditTextLogin.getText().toString();
                 String passwd = mEditTextPassword.getText().toString();
+
+                if(mServerAddressEditText.getVisibility() == View.VISIBLE)
+                    mSelectedAddress = mServerAddressEditText.getText().toString();
 
                 if (login.length() == 0) {
                     Toast.makeText(
@@ -98,7 +132,15 @@ public class LoginFragment extends Fragment implements RequestCallback {
                             getResources().getText(R.string.login_fragment_empty_passwd),
                             Toast.LENGTH_LONG).show();
                     return;
+                } else if (mSelectedAddress.length() == 0 || !mSelectedAddress.startsWith("http://")) {
+                    Toast.makeText(
+                            getActivity(),
+                            getResources().getText(R.string.login_fragment_address_format_error),
+                            Toast.LENGTH_LONG).show();
+                    return;
                 }
+
+                HomecloudHolder.getInstance().setUrl(mSelectedAddress);
 
                 Toast.makeText(getActivity(), "Logging in...", Toast.LENGTH_SHORT).show();
 
